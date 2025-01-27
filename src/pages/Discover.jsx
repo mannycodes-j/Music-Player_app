e00a55efde19d38fd9f3cef724e8724a285a3b6e
playDev11 <play.dev6@gmail.com>
2025-01-27 06:29:34 -0800

    feat: initialize project structure with basic components and configuration

---
+++ src/pages/Discover.jsx
@@ -0,0 +1,33 @@
+import { Error, Loader, Songcard } from '../components '
+import { genres } from '../assets/constants'
+
+const Discover = () => {
+  console.log(genres)
+  const genreTitle = 'pop'
+
+  return (
+    <div className="flex flex-col">
+      <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+        <h2 className="font-bold text-3xl text-white text-left">
+          Discover {genreTitle}
+        </h2>
+        <select
+          onChange={() => {}}
+          value=""
+          className="bg-black text-gray-300 p-3 rounded-lg outline-none text-sm sm:mt-0 mt-5"
+        >
+          {genres.map((genre) => (
+            <option key={genre.value} value={genre.value}>
+              {genre.title}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Discover
